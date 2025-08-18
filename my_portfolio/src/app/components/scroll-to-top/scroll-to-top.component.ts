9f15ae889321fb5713e4a737388c9b0f7412b2a6
E-adebayo <dalmeud@gmail.com>
2025-08-19 01:10:30 +0200

    Update before deployment

--- my_portfolio/src/app/components/scroll-to-top/scroll-to-top.component.ts
+++ my_portfolio/src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -1,15 +1,18 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-scroll-to-top',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FontAwesomeModule],
     templateUrl: './scroll-to-top.component.html',
     styleUrls: ['./scroll-to-top.component.scss']
 })
 export class ScrollToTopComponent implements OnInit {
     isVisible = false;
+    faArrowUp = faArrowUp;
     
 
     ngOnInit(): void {
@@ -25,6 +28,16 @@ export class ScrollToTopComponent implements OnInit {
     }
 
     scrollToTop() {
+        // Add ripple effect
+        const button = document.querySelector('.scroll-to-top-btn');
+        if (button) {
+            button.classList.add('clicked');
+            setTimeout(() => {
+                button.classList.remove('clicked');
+            }, 600);
+        }
+
+        // Smooth scroll to top
         window.scrollTo({ top: 0, behavior: 'smooth' });
         // The visibility will be handled by the checkScroll method which is triggered by the scroll event
     }
